4bb29476566013d91567a024bbe158620f446f03
Mirea26 <m.cernota@hbk-bs.de>
2023-12-04 14:00:41 +0100

    Slider und Formen noch ausbaufähig
    Slider: Wenn man ihn bewegt, wird es größer ABER nicht mehr kleiner
    Formen: Zweiter Button funktioniert nicht. Warum?

--- sketch.js
+++ sketch.js
@@ -4,19 +4,28 @@ const saveButton = document.querySelector('button#save');
 const colorInput = document.querySelector('input#color');
 console.log(colorInput);
 const rectButton = document.querySelector('button#rect');
-// I added the slider by selecting the element in the DOM
-// that you added.
 const slider = document.querySelector('input#slider');
+const triangleButton = document.querySelector('button#triangle');
 
 let paintColor = '#ff6347';
 colorInput.value = paintColor;
 
-let sliderValue = 1;
+let sliderValue = 10;
+//slider.value = sliderValue;
+let size = 10;
+const step = 5;
+slider.value = size;
 
 // these are some names for your different drawing styles
 const drawType = ['rectangle', 'circle'];
 // this is for selecting one of your drawing styles
 let drawSelectedIndex = 0;
+
+const drawTypeII = ['triangle', 'ellipse'];
+
+let drawSelectedIndexII = 0;
+
+
 function setup() {
   const canvas = createCanvas(200, 200);
   canvas.parent('sketch');
@@ -48,45 +57,94 @@ function setup() {
     // the slider apply it to your strokeWeight or something else
     // you need to do the same as for the paintColor
     // you have to create a global variable that you can overwrite
+    
+    //if(slider.value > min){
+    size = size + step; 
+    //size = sliderValue;
     console.log(slider.value);
-  })
+    //sliderValue = slider.value;
+    //}
+    //else{
+        //size = size - step;
+    //}
 
+    //Gibt es einen Befehl bei dem man sagen kann, dass wenn man den Slider nach rechts bewegt,
+    //der Strich immer größer wird (d.h. + step) und wenn man ihn nach links bewegt, der Strich 
+    //wieder kleiner wird (d.h. -step)???
+
+  })
+    
+  triangleButton.addEventListener('cklick', ()=> {
+    console.log('clicked');
+    drawSelectedIndexII++;
+    drawSelectedIndexII = drawSelectedIndexII % drawTypeII.length
+    triangleButton.innerText = drawTypeII[drawSelectedIndexII]
+  })
 }
 
 function draw() {}
 
 function mouseDragged() {
-  // fill(paintColor);
-  // noStroke();
-  // circle(mouseX, mouseY, 20);
-  // line(pmouseX, pmouseY, mouseX, mouseY);
+   fill(paintColor);
+   noStroke();
+   circle(mouseX, mouseY, size);
+   line(pmouseX, pmouseY, mouseX, mouseY);
 
   // stroke(paintColor);
   // strokeWeight(sliderValue);
   // line(pmouseX, pmouseY, mouseX, mouseY);
   // for (let i = 0; i < 100; i++) {}
-  fill(paintColor);
+  //fill(paintColor);
 
   // this is a switch statement
   // it is better suited for your case here then
   // writing a lot of if statements
   // but it does actually the same thing
   // see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/switch
-  switch (drawType[drawSelectedIndex]) {
-    case 'rectangle':
-      rect(mouseX, mouseY, 10, 10);
-      break;
-    case 'circle':
-      circle(mouseX, mouseY, 10);
-      break;
-    default:
-      console.log('not defined state for drawing')
-      break;
-  }
+    
 }
 
-function mouseClicked() {
 
+function mouseClicked() {
+    //if(rectButton('clicked')){
+    
+    fill(paintColor);
+    noStroke();
+    switch (drawType[drawSelectedIndex]) {
+        case 'rectangle':
+          rect(mouseX, mouseY, size, size);
+          break;
+        case 'circle':
+          circle(mouseX, mouseY, size);
+          break;
+        default:
+          console.log('not defined state for drawing')
+          break;
+      }
+      
+      //}
+    //else if(triangleButton('clicked')){
+        //fill(paintColor);
+
+    switch (drawtypeII[drawSelectedIndexII]){
+        case 'triangle':
+        triangle(mouseX, mouseY, mouseX - size, mouseY + size, mouseX + size, mouseY +size);
+        break;
+        case 'ellipse':
+        ellipse(mouseX, mouseY, size, size);
+        break;
+        default:
+            console.log('not defined state for drawing')
+        break;
+    }
+
+    //}
+    //else {
+        //fill(paintColor);
+        //noStroke();
+        //circle(mouseX, mouseY, size);
+        //line(pmouseX, pmouseY, mouseX, mouseY);
+    //}
 }
 
 function keyPressed() {
